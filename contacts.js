37b41dbe14267f4ff5cf98d84e9defd59cf6f1f4
Titotrapatonni <titovtelexfree@gmail.com>
2023-05-07 15:55:20 +0300

    connect contacts json

---
+++ contacts.js
@@ -0,0 +1,6 @@
+const fs = require("fs/promises");
+const path = require("path");
+const contacts = require("./db/contacts.json");
+console.log("====================================");
+console.log(contacts);
+console.log("====================================");
